f7f36957bae40fbb79b816571d8e801f17926a5a
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2023-02-27 17:50:22 -0300

    Add files via upload

---
+++ Tema_2-Sintaxis,variables-y-palabras reservadas de JS-Ejercicio_1.js
@@ -0,0 +1,10 @@
+const fecha = new Date(1995, 11, 16);
+
+const libro = {
+    titulo: "El señor de los anillos",
+    autor: "J. R. R. Tolkien",
+    fecha: new Date(1954, 0, 1),
+    url: "https://www.amazon.com/-/es/J-R-Tolkien/dp/844501384X"
+}
+
+const lista = ["Maitén", 27, false, fecha, ];
