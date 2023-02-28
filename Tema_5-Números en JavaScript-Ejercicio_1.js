767d37ea3f04cd2ba90203f84efcb77c1561a245
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2023-02-28 18:29:53 -0300

    Add files via upload

---
+++ Tema_5-Números en JavaScript-Ejercicio_1.js
@@ -0,0 +1,14 @@
+let altura_cm = 165;
+
+let altura_m = 1.65;
+
+let peso = 52.5;
+
+let altura_redondeada_arriba = Math.ceil(altura_m);
+console.log(altura_redondeada_arriba);
+
+let peso_redondeado_abajo = Math.floor(peso);
+console.log(peso_redondeado_abajo);
+
+let variables_iguales = (Number.MAX_VALUE + 1 === Number.MAX_VALUE);
+console.log(variables_iguales);
