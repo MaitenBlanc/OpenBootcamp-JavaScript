38df45f827f62e54617b9824c13f8751e205ce7b
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2023-02-28 11:25:47 -0300

    Add files via upload

---
+++ Tema_3 - Estructuras de control - Ejercicio_1/factorial-for.js
@@ -0,0 +1,7 @@
+let factorial = 1;
+
+for (let i = 10; i > 0; i--) {
+    factorial = factorial * i;
+}
+
+console.log(factorial);
---
+++ Tema_3 - Estructuras de control - Ejercicio_1/factorial-while.js
@@ -0,0 +1,9 @@
+let factorial = 1;
+let i = 10;
+
+while (i > 1) {
+    factorial = factorial * i;
+    i--;
+}
+
+console.log(factorial);
