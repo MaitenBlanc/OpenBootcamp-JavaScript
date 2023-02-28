df5f84ba4012652f541be3ec117d6b3c5404d1bc
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2023-02-28 11:26:32 -0300

    Delete Tema_3 - Estructuras de control - Ejercicio_1 directory

--- Tema_3 - Estructuras de control - Ejercicio_1/factorial-for.js
+++
@@ -1,7 +0,0 @@
-let factorial = 1;
-
-for (let i = 10; i > 0; i--) {
-    factorial = factorial * i;
-}
-
-console.log(factorial);
--- Tema_3 - Estructuras de control - Ejercicio_1/factorial-while.js
+++
@@ -1,9 +0,0 @@
-let factorial = 1;
-let i = 10;
-
-while (i > 1) {
-    factorial = factorial * i;
-    i--;
-}
-
-console.log(factorial);
